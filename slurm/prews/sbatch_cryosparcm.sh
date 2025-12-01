033866b80e1c2174696651d058e3ba8d289db250
Tatsuya IKUTA <tatsuya.ikuta@gmail.com>
2025-12-01 12:25:38 +0900

    Change to backup first

--- slurm/prews/sbatch_cryosparcm.sh
+++ slurm/prews/sbatch_cryosparcm.sh
@@ -7,23 +7,32 @@
 #SBATCH --mem=32G # 32/256 < 1/8 of the full node
 #SBATCH -o sbatch_cryosparcm.out
 #SBATCH --open-mode=append
-#SBATCH -d afterok:1955
+#SBATCH -d afterok:1999
 #SBATCH --nodelist=prews-cpu07
 
 CSM="/home/tikuta/local/src/cryosparc_master/bin/cryosparcm"
 RCLONE="/home/tikuta/local/src/rclone-v1.71.2-linux-amd64/rclone"
 
+START=$(date -u +%s)
+
 echo "CryoSPARC master is running on ${HOSTNAME}"
 sed -i -e "s/^export CRYOSPARC_MASTER_HOSTNAME=.*$/export CRYOSPARC_MASTER_HOSTNAME=\"${HOSTNAME}\"/g"  ~/local/src/cryosparc_master/config.sh
 echo "CryoSPARC master started at $(date -u +%s)"
 $CSM start
-echo "Sleep for a day..."
-sleep $((1 * 24 * 60 * 60))
+
 echo "CryoSPARC master backup started at $(date -u +%s)"
 $CSM backup
 echo "CryoSPARC master backup ended at $(date -u +%s)"
-$CSM stop
-echo "CryoSPARC master ended at $(date -u +%s)"
 echo "Rclone started at $(date -u +%s)"
 $RCLONE copy /home/tikuta/local/src/cryosparc_database/backup pCloud:/cryosparc_db_backup/tikuta@prews
 echo "Rclone ended at $(date -u +%s)"
+
+TIME_LIMIT=$((36 * 60 * 60)) # Job time limit set above
+NOW=$(date -u +%s)
+WAIT=$((15 * 60)) # 15 min for cryosparcm to stop
+SLEEP_TIME=$(($START + $TIME_LIMIT - $NOW - $WAIT))
+
+echo "Sleep for $(($SLEEP_TIME / 60 / 60)) hours ($SLEEP_TIME seconds)"
+sleep $SLEEP_TIME
+$CSM stop
+echo "CryoSPARC master ended at $(date -u +%s)"
